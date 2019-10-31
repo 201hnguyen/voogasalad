581715f5b75ca64f52a0bf36802dec0f7827b391
Robert Duvall <rcd@cs.duke.edu>
2019-10-31 18:05:42 -0400

    Added initial project files

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module voogasalad_WorksDontTouch {
+    requires java.xml;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.web;
+
+    exports voogasalad;
+}
