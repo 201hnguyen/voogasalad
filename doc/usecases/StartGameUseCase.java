c172d7e6953cf0350c99904174be89c23bc80b08
Sumer Vardhan <sv110@duke.edu>
2019-11-09 00:00:38 -0500

    Final submission for planning stage

--- doc/usecases/StartGameUseCase.java
+++ doc/usecases/StartGameUseCase.java
@@ -1,6 +1,7 @@
 package useCases;
 
 public class GamePlayerUseCase{
+    //This object is created elsewhere
     GamePlayer obj = new GamePlayer();
     obj.loadXML(String xmlPath);
     obj.startGame();
