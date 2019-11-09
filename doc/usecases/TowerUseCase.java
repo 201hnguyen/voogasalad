fe899d1628d068a79060346c801e258daaf0fed0
Xiaoyang Liu <xiaoyang.liu@duke.edu>
2019-11-08 22:19:18 -0500

    example tower data

--- doc/usecases/TowerUseCase.java
+++
@@ -1,4 +0,0 @@
-//Tower use case 1. editing base stats for a new type of tower
-Display myGAE = new Display();
-Tower newTowerType = myGAE.editTowers();
-myGAE.GameConfiguration.addTower(newTowerType);
