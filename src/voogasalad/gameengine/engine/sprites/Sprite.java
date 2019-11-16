3fa957042ff1249d492e43dd94e5e4b73fc83d35
Ha Nguyen <hn47@duke.edu>
2019-11-15 19:14:29 -0500

    Removed unused import statement

--- src/voogasalad/gameengine/engine/sprites/Sprite.java
+++ src/voogasalad/gameengine/engine/sprites/Sprite.java
@@ -1,7 +1,6 @@
 package voogasalad.gameengine.engine.sprites;
 
 import voogasalad.gameengine.engine.exceptions.GameEngineException;
-import voogasalad.gameengine.engine.sprites.strategies.health.HealthStrategy;
 
 public interface Sprite {
     Sprite makeClone(int x, int y, int spriteId) throws GameEngineException;
