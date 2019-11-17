5a382ef41ac14fd9ba753a45364a282cd9210874
Ha Nguyen <hn47@duke.edu>
2019-11-16 20:11:40 -0500

    ISSUE-21: Users can now specify intervals between when sprites are spawned in a wave

--- src/voogasalad/gameengine/engine/gamecontrol/Wave.java
+++ src/voogasalad/gameengine/engine/gamecontrol/Wave.java
@@ -1,19 +1,43 @@
 package voogasalad.gameengine.engine.gamecontrol;
 
+import voogasalad.gameengine.engine.exceptions.GameEngineException;
+
 import java.awt.Point;
 import java.util.Queue;
 
 public class Wave {
     Queue<Integer> mySpritesQueue;
+    Queue<Integer> mySpritesEntryTimeQueue;
     Point mySpawnPoint;
+    double myTotalElapsedTime;
+    double myNextEntryTime;
 
-    public Wave(Queue<Integer> spritesQueue, Point spawnPoint) {
+    public Wave(Queue<Integer> spritesQueue, Queue<Integer> spritesEntryTimeQueue, Point spawnPoint) throws GameEngineException {
+        if (spritesAndEntryQueueSizesMismatch(spritesQueue, spritesEntryTimeQueue)) {
+            throw new GameEngineException("SpritesAndEntryQueueSizesMismatch");
+        }
+        myTotalElapsedTime = 0;
         mySpritesQueue = spritesQueue;
+        mySpritesEntryTimeQueue = spritesEntryTimeQueue;
+        myNextEntryTime = 0;
         mySpawnPoint = spawnPoint;
     }
 
-    public Integer getNextSpriteToSpawn() {
-        return mySpritesQueue.remove();
+    public Integer getNextSpriteToSpawn(double elapsedTime){
+        myTotalElapsedTime +=elapsedTime;
+        if (myTotalElapsedTime >= myNextEntryTime) {
+            return mySpritesQueue.remove();
+        }
+        return null;
+    }
+
+    public void setNextEntryTime() throws GameEngineException {
+        if (mySpritesEntryTimeQueue.size()!=0 && myNextEntryTime<=myTotalElapsedTime) {
+            myNextEntryTime = mySpritesEntryTimeQueue.remove();
+            if (myNextEntryTime <= myTotalElapsedTime) {
+                throw new GameEngineException("InvalidTimeSpecifiedForSpriteEntry");
+            }
+        }
     }
 
     public Point getSpawnPoint() {
@@ -22,5 +46,9 @@ public class Wave {
 
     public boolean isEmpty() {
         return mySpritesQueue.size() == 0;
+    }
+
+    private boolean spritesAndEntryQueueSizesMismatch(Queue<Integer> spritesQueue, Queue<Integer> spritesEntryTimeQueue) {
+        return spritesQueue.size() != spritesEntryTimeQueue.size()+1;
     }
 }
