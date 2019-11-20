51f53d49be180732a2c154bd91de66cf1a2c68d6
Ha Nguyen <ha.nguyen@duke.edu>
2019-11-19 23:18:19 -0500

    ISSUE-(compsci308_2019fall/voogasalad_worksdonttouch#63): Fixed bug in make clone that affected display animation

--- src/voogasalad/gameengine/engine/sprites/SpriteManager.java
+++ src/voogasalad/gameengine/engine/sprites/SpriteManager.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface SpriteManager {
     void addSpritePrototype(int prototypeId, Sprite sprite);
     void makeSpriteFromPrototype(double xPos, double yPos, int prototypeId) throws GameEngineException;
-    List<JavaFXSprite> getOnScreenSprites();
+    List<Sprite> getOnScreenSprites();
     void removeSpriteById(int spriteId);
     }
