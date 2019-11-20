b080d71bd58a92fbea004665ca81e7143e9025a3
Michael Castro <mc546@duke.edu>
2019-11-19 20:24:29 -0500

    Front end GUI

---
+++ src/player/GUI/AccordionCreator.java
@@ -0,0 +1,26 @@
+package player;
+        import javafx.scene.control.Accordion;
+        import javafx.scene.control.TitledPane;
+        import javafx.scene.image.Image;
+        import javafx.scene.image.ImageView;
+        import java.util.Collections;
+        import java.util.ResourceBundle;
+
+public class AccordionCreator extends Accordion {
+    private static final String RESOURCE_PATH = "resources.player.AccordionResource";
+    private ResourceBundle resourceBundle;
+
+    public AccordionCreator() {
+        resourceBundle = ResourceBundle.getBundle(RESOURCE_PATH);
+        createAccordion();
+    }
+
+    private void createAccordion() {
+        getChildren().clear();
+        for (String key : Collections.list(resourceBundle.getKeys())) {
+            ImageView imageView = new ImageView(new Image(resourceBundle.getString(key), true));
+            TitledPane pane = new TitledPane(key, imageView);
+            getPanes().add(pane);
+        }
+    }
+}
---
+++ src/player/GUI/PlayerVisualization.java
@@ -0,0 +1,54 @@
+package player;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class PlayerVisualization {
+
+    private static final double SCENE_WIDTH = 830;
+    private static final double SCENE_HEIGHT = 630;
+    private static final double PANEL_POSITION = 700;
+    private static final double LAYOUT = 0;
+    private static final String TITLE = "Player";
+
+    private Scene scene;
+    private Stage stage;
+
+    public PlayerVisualization(Stage stage) {
+        this.stage = stage;
+        initialize();
+    }
+
+    private void startStage() {
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle(TITLE);
+        stage.show();
+    }
+
+    public void initialize() {
+        Pane root = new Pane();
+        ButtonCreator buttonCreator = new ButtonCreator();
+        AccordionCreator accordionCreator = new AccordionCreator();
+        VBox panelBox = new VBox();
+        panelBox.getChildren().add(buttonCreator);
+        panelBox.getChildren().add(accordionCreator);
+        panelBox.setLayoutX(PANEL_POSITION);
+        Pane displayScreen = displayScreen();
+        root.getChildren().addAll(displayScreen, panelBox);
+        scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        startStage();
+    }
+
+    private Pane displayScreen() {
+        Pane displayScreen = new Pane();
+        displayScreen.setMinWidth(PANEL_POSITION);
+        displayScreen.setMinHeight(SCENE_HEIGHT);
+        displayScreen.setLayoutX(LAYOUT);
+        displayScreen.setLayoutY(LAYOUT);
+        displayScreen.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        return displayScreen;
+    }
+}
