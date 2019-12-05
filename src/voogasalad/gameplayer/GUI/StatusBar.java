fdfcc193f3ca268fdb4835f985b7b7cdf696c68f
Ben Keegan <bk142@duke.edu>
2019-12-05 14:12:47 -0500

    status bar info tracker box

--- src/voogasalad/gameplayer/GUI/StatusBar.java
+++ src/voogasalad/gameplayer/GUI/StatusBar.java
@@ -1,26 +1,52 @@
 package voogasalad.gameplayer.GUI;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 import java.util.Map;
+import java.util.Set;
 
 public class StatusBar extends HBox {
 
+    private Map<String, Integer> myGameInfoMap;
+
     public void StatusBar(Map<String, Integer> gameInfoMap) {
+        myGameInfoMap = gameInfoMap;
+        for(String key : gameInfoMap.keySet()) {
+            this.getChildren().add(infoTrackerBox(key, gameInfoMap.get(key)));
+        }
+    }
+
+    private HBox infoTrackerBox(String infoName, int infoValue) {
+        // need to add icon and value
+        HBox infoBox = new HBox();
+        ImageView icon = iconLoader(infoName);
+
+        Text value = new Text();
+        value.setText(String.valueOf(infoValue));
+
+        infoBox.getChildren().add(icon);
+        infoBox.getChildren().add(value);
+        return infoBox;
+    }
+
+    public void updateDisplayedInfo() {
 
     }
 
-    private Pane coinTracker() {
-
-    }
-
-    private Pane livesTracker() {
-
-    }
-
-    private ImageView iconLoader() {
-
+    /**
+     *
+     * @param iconIdentifier - key from the gameInfoMap such as Lives or Coins
+     * @return image view of the icon associated with the key
+     */
+    private ImageView iconLoader(String iconIdentifier) {
+        iconIdentifier = iconIdentifier.toLowerCase();
+        ImageView icon = new ImageView(new Image(iconIdentifier));
+        icon.setFitHeight(30);
+        icon.setFitWidth(30);
+        return icon;
     }
 }
