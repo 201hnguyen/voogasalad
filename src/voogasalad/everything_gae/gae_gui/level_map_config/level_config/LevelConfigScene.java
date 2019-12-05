8fd3b63c56efbb74468ae1b0da49795b046b64af
Marc Jabbour <marc.jabbour@duke.edu>
2019-12-05 11:58:36 -0500

    Created separate class for 'Rule Lines'

--- src/voogasalad/everything_gae/gae_gui/level_map_config/level_config/LevelConfigScene.java
+++ src/voogasalad/everything_gae/gae_gui/level_map_config/level_config/LevelConfigScene.java
@@ -17,6 +17,7 @@ public class LevelConfigScene{
 
     public Scene getScene(BorderPane root){
         ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToWidth(true);
         GridPane gridPane = new GridPane();
         HBox title = createTitleHBox();
         HBox allObjects = createAllObjectHBox();
@@ -41,20 +42,45 @@ public class LevelConfigScene{
 
     public HBox createAllObjectHBox(){
         HBox allObjectHBox = new HBox();
+        ObjectVBox towersVBox = new ObjectVBox("Towers", width, height);
+        ObjectVBox enemiesVBox = new ObjectVBox("Enemies", width, height);
+        ObjectVBox obstaclesVBox = new ObjectVBox("Obstacles", width, height);
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        towersVBox.addToObjectHBox(new Button("test"));
+//        enemiesVBox.addToObjectHBox(new Button("test"));
+//        enemiesVBox.addToObjectHBox(new Button("test"));
+//        enemiesVBox.addToObjectHBox(new Button("test"));
+//        obstaclesVBox.addToObjectHBox(new Button("test"));
+//        obstaclesVBox.addToObjectHBox(new Button("test"));
+//        obstaclesVBox.addToObjectHBox(new Button("test"));
         allObjectHBox.setPrefHeight(height/5);
-        allObjectHBox.getChildren().addAll(new ObjectVBox("Towers", width, height), new ObjectVBox("Enemies", width, height), new ObjectVBox("Obstacles", width, height));
+        allObjectHBox.getChildren().addAll(towersVBox, enemiesVBox, obstaclesVBox);
         return allObjectHBox;
     }
 
     public BorderPane createRulesVBox(){
         BorderPane borderPane = new BorderPane();
-        Label rulesLabel = new Label("Rules");
-        VBox conditionAction = new VBox(5);
-        HBox line = new HBox();
-        line.getChildren().addAll(new Label("Condition: "), new ComboBox(), new Label("  ---> Action: "), new ComboBox());
-        conditionAction.getChildren().add(line);
+        Label rulesLabel = new Label("Rules: \n");
+        rulesLabel.setFont(Font.font(20));
+        VBox conditionAction = new VBox(10);
+        conditionAction.getChildren().add(new RuleLine());
+        Button addRuleLine = new Button("+");
+        addRuleLine.setOnMouseClicked(event -> {
+            conditionAction.getChildren().add(new RuleLine());
+        });
         borderPane.setTop(rulesLabel);
         borderPane.setCenter(conditionAction);
+        borderPane.setBottom(addRuleLine);
         return borderPane;
     }
 
--- src/voogasalad/everything_gae/gae_gui/level_map_config/level_config/ObjectVBox.java
+++ src/voogasalad/everything_gae/gae_gui/level_map_config/level_config/ObjectVBox.java
@@ -3,19 +3,21 @@ package voogasalad.everything_gae.gae_gui.level_map_config.level_config;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class ObjectVBox extends VBox {
-    HBox createdObjects;
+    FlowPane createdObjects;
 
     public ObjectVBox(String type, int width, int height){
         super(10);
         Label createdLabel = new Label("Created " + type);
         createdLabel.setTextFill(Color.CHOCOLATE);
-        createdObjects = new HBox(5);
+        createdObjects = new FlowPane();
+        createdObjects.setAlignment(Pos.CENTER);
         this.setMaxHeight(height/5);
         this.setAlignment(Pos.TOP_CENTER);
         this.setPrefWidth(width/3);
