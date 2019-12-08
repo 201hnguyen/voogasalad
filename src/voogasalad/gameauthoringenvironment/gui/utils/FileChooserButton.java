c9fe8167823f9d17a8e3a82c61564ac1d6ba1242
Amber Johnson <ajj18@duke.edu>
2019-12-07 19:45:02 -0500

    issue #145 minor progress

--- src/voogasalad/gameauthoringenvironment/gui/utils/FileChooserButton.java
+++ src/voogasalad/gameauthoringenvironment/gui/utils/FileChooserButton.java
@@ -1,4 +1,4 @@
-package voogasalad.gameauthoringenvironment.gui.tabconfig;
+package voogasalad.gameauthoringenvironment.gui.utils;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
@@ -10,18 +10,36 @@ import javafx.stage.Stage;
 import java.io.File;
 
 public class FileChooserButton extends Button {
+
     private Stage newStage = new Stage();
+    private ImageView imageView;
+    private String selectedImage = "";
 
     public FileChooserButton(){
         super("Select Image");
+        //this.imageView = getImageView();
+        createButton();
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    //TODO: add exception
+    private void createButton() {
         final FileChooser imageChooser = new FileChooser();
         this.setOnAction((final ActionEvent e) -> {
             File file = imageChooser.showOpenDialog(newStage);
             if (file != null) {
-                Image image1 = new Image(file.toURI().toString());
-                ImageView ip = new ImageView(image1);
-                ip.setFitWidth(500);
-                ip.setFitHeight(500);
+                Image image = new Image(file.toURI().toString());
+                imageView = new ImageView(image);
+                //imageView.setFitWidth(500);
+                //imageView.setFitHeight(500);
             }
         });
     }
