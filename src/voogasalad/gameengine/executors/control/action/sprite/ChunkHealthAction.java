11a0bd43abed4a8a50398a24e21edc7fec483845
Chris Xu <christopher.xu@duke.edu>
2019-12-07 19:30:00 -0500

    (Issue #147) Create ChunkHealthAction implementing SpriteAction

--- src/voogasalad/gameengine/executors/control/action/sprite/ChunkHealthAction.java
+++ src/voogasalad/gameengine/executors/control/action/sprite/ChunkHealthAction.java
@@ -1,4 +1,15 @@
 package voogasalad.gameengine.executors.control.action.sprite;
+import voogasalad.gameengine.executors.sprites.Sprite;
 
-public class ChunkHealthAction {
+public class ChunkHealthAction implements SpriteAction {
+    private int damageValue;
+
+    public ChunkHealthAction(int damage) {
+        damageValue = damage;
+    }
+
+    @Override
+    public void execute(Sprite sprite) {
+        sprite.chunkHealth(damageValue);
+    }
 }
