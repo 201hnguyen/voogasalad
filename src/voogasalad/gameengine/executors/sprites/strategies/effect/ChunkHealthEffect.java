6ce3b627fabb106143017d7c0aaaa1d3b15c9753
Chris Xu <christopher.xu@duke.edu>
2019-12-07 23:52:03 -0500

    (Issue #155) Added isFinished flag to EffectStrategy

--- src/voogasalad/gameengine/executors/sprites/strategies/effect/ChunkHealthEffect.java
+++ src/voogasalad/gameengine/executors/sprites/strategies/effect/ChunkHealthEffect.java
@@ -7,15 +7,23 @@ import voogasalad.gameengine.executors.sprites.Sprite;
 public class ChunkHealthEffect implements EffectStrategy {
     private EffectBuilder myOriginalBuilder;
     private int damageValue;
+    private boolean isFinished;
 
     public ChunkHealthEffect(EffectBuilder builder) {
         damageValue = builder.getDamage();
         myOriginalBuilder = builder;
+        isFinished = false;
     }
 
     @Override
     public void apply(Sprite sprite) throws GameEngineException {
         sprite.chunkHealth(damageValue);
+        isFinished = true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return isFinished;
     }
 
     @Override
