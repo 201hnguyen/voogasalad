091ea1538e8d05bb406d8c3e60fb9e023ebc55c5
Marc Jabbour <marc.jabbour@duke.edu>
2019-12-08 17:50:24 -0500

    GameLevelComboBox properly showing all created levels ... now need to add functionality to update

--- src/voogasalad/gameauthoringenvironment/gui/levelconfig/nodes/GameLevelComboBox.java
+++ src/voogasalad/gameauthoringenvironment/gui/levelconfig/nodes/GameLevelComboBox.java
@@ -1,34 +1,37 @@
 package voogasalad.gameauthoringenvironment.gui.levelconfig.nodes;
 
-        import javafx.beans.value.ChangeListener;
-        import javafx.beans.value.ObservableValue;
-        import javafx.scene.control.ComboBox;
-        import voogasalad.gameauthoringenvironment.gui.levelconfig.LevelConfigPane;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.ComboBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameLevelComboBox extends ComboBox {
     private String current;
     private List<Integer> allLevels;
+    private String currentLevel;
 
     public GameLevelComboBox(){
         allLevels = new ArrayList<>();
         this.setValue("1");
-//        this.valueProperty().addListener(new ChangeListener() {
-//            @Override
-//            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-//                updateLevelConfigFields(Integer.parseInt(newValue.toString()));
-//            }
-//        });
+        this.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ComboBox thisInstance = (ComboBox) event.getSource();
+                String currentVal = thisInstance.getValue().toString();
+                updateLevelConfigFields(currentVal);
+            }
+        });
     }
 
-    public void updateLevelConfigFields(int newValue){
-        //Load New
+    public void updateLevelConfigFields(String newValue){
         System.out.println("Test: " + newValue);
     }
 
-    public void addToComboBox(int previousLevel, int currentLevel){
+    public void addToComboBox(int previousLevel, int currentLevelParam){
+        currentLevel = String.valueOf(currentLevelParam);
         allLevels.add(previousLevel);
         this.setValue(String.valueOf(currentLevel));
         this.getItems().add(previousLevel);
