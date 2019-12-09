0d0d3eb7b08f042bdafbc4535122384c6153e206
Chris Xu <christopher.xu@duke.edu>
2019-12-08 22:59:49 -0500

    (Issue #181) Fixed bug in PathMovement referencing the wrong path

--- src/voogasalad/gameengine/executors/control/gamecontrol/Game.java
+++ src/voogasalad/gameengine/executors/control/gamecontrol/Game.java
@@ -33,7 +33,7 @@ public class Game {
         myStatus = Status.ONGOING;
         myGameRulesController = new GameRulesController();
         myGameActionsRequester = new GameActionsRequester();
-        myGameConfigDocument = ConfigurationTool.configureWithTestDocument("src/resources/player/MockGame.xml");
+        myGameConfigDocument = ConfigurationTool.configureWithTestDocument("src/resources/player/MockData.xml");
         GameConfigurator gameConfigurator = new GameConfigurator(myGameConfigDocument);
         myCompletePrototypesCollection = gameConfigurator.getGamePrototypesCollection();
         myGameRulesController.addGameConditionsAsCollection(gameConfigurator.configureGameConditions());
