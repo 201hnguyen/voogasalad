a313e294f7b142951be3e9d13692a7d7b0476817
Amber Johnson <ajj18@duke.edu>
2019-12-08 22:05:54 -0500

    johnson5 tabvboxcreator updated

--- src/voogasalad/gameauthoringenvironment/gui/utilconfig/TabVBoxCreator.java
+++ src/voogasalad/gameauthoringenvironment/gui/utilconfig/TabVBoxCreator.java
@@ -2,16 +2,27 @@ package voogasalad.gameauthoringenvironment.gui.utilconfig;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
+
 public class TabVBoxCreator extends VBox{
 
-    public TabVBoxCreator(String vBoxName, double width, double top, double right, double bottom, double left) {
+    ScrollPane myScrollPane;
+
+    public TabVBoxCreator(String vBoxName, Priority priority, double width, double top, double right, double bottom, double left) {
+        myScrollPane = new ScrollPane();
+        this.setVgrow(myScrollPane, priority);
         Label header = new Label(vBoxName);
         header.setFont(Font.font(14));
-        this.getChildren().add(header);
+        this.getChildren().addAll(header, myScrollPane);
         this.setPrefWidth(width);
         this.setPadding(new Insets(top, right, bottom, left));
+    }
+
+    public ScrollPane getScrollPane() {
+        return myScrollPane;
     }
 }
