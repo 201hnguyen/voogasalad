d889eb8cbcab209b4d5328cbcbca37852e0d8289
Emily Otero <eo56@duke.edu>
2019-12-15 17:23:53 -0500

    added javadoc to health and cost builders

--- src/voogasalad/gameengine/executors/objectcreators/CostBuilder.java
+++ src/voogasalad/gameengine/executors/objectcreators/CostBuilder.java
@@ -5,6 +5,9 @@ import voogasalad.gameengine.executors.sprites.strategies.cost.CostStrategy;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Parses cost parameters from the XML and builds a new cost strategy using those parameters.
+ */
 public class CostBuilder implements StrategyBuilder{
 
     public static final String DEFAULT_TYPE = "BasicCost";
@@ -15,17 +18,34 @@ public class CostBuilder implements StrategyBuilder{
     private String myType;
 
 
+    /**
+     * Sets create cost and destroy cost to the same value
+     * @param cost string that represents the base cost of creating and destroying a sprite
+     * @return self
+     * @throws GameEngineException
+     */
     public CostBuilder setCost(String cost) throws GameEngineException {
         setCreateCost(cost);
         setDestroyCost(cost);
         return this;
     }
 
+    /**
+     * Saves string that represents type of cost strategy to be built
+     * @param type string that represents type of cost strategy to be built
+     * @return self
+     */
     public CostBuilder setType(String type) {
         myType = type;
         return this;
     }
 
+    /**
+     * Parses cost string. Saved as create cost
+     * @param cost string that represents the cost to the player for creating the sprite
+     * @return
+     * @throws GameEngineException
+     */
     public CostBuilder setCreateCost(String cost) throws GameEngineException {
         try {
             myCreateCost = Integer.parseInt(cost);
@@ -35,6 +55,12 @@ public class CostBuilder implements StrategyBuilder{
         return this;
     }
 
+    /**
+     * Parses cost string. Saved as destroy cost
+     * @param cost string that represents the cost returned to the player after destroying the sprite
+     * @return self
+     * @throws GameEngineException
+     */
     public CostBuilder setDestroyCost(String cost) throws GameEngineException {
         try {
             myDestroyCost = Integer.parseInt(cost);
@@ -44,14 +70,25 @@ public class CostBuilder implements StrategyBuilder{
         return this;
     }
 
+    /**
+     * @return current set create cost; no default is set
+     */
     public int getCreateCost() {
         return myCreateCost;
     }
 
+    /**
+     * @return current set destroy cost; no default is set
+     */
     public int getDestroyCost() {
         return myDestroyCost;
     }
 
+    /**
+     * Calls the constructor of the type it's meant to handle; uses default when no type is set
+     * @return a strategy of the specified type with the parameters previously parsed
+     * @throws GameEngineException
+     */
     @Override
     public CostStrategy build() throws GameEngineException {
         if (myType == null) {
--- src/voogasalad/gameengine/executors/objectcreators/HealthBuilder.java
+++ src/voogasalad/gameengine/executors/objectcreators/HealthBuilder.java
@@ -3,6 +3,9 @@ package voogasalad.gameengine.executors.objectcreators;
 import voogasalad.gameengine.executors.exceptions.GameEngineException;
 import voogasalad.gameengine.executors.sprites.strategies.health.HealthStrategy;
 
+/**
+ * Parses health parameters from the XML and builds a new cost strategy using those parameters.
+ */
 public class HealthBuilder implements StrategyBuilder {
     private static final String CLASS_PATH = "voogasalad.gameengine.executors.sprites.strategies.health.";
     public static final String DEFAULT_TYPE = "NoHealth";
@@ -10,11 +13,22 @@ public class HealthBuilder implements StrategyBuilder {
     private int myHealthValue;
     private String myType;
 
+    /**
+     * Saves string type
+     * @param type string that represents type of cost strategy to be built
+     * @return self
+     */
     public HealthBuilder setType(String type) {
         myType = type;
         return this;
     }
 
+    /**
+     * Parses string that represents the starting health of the sprite
+     * @param healthValue string that represents the starting health of the sprite
+     * @return self
+     * @throws GameEngineException
+     */
     public HealthBuilder setHealthValue(String healthValue) throws GameEngineException {
         try {
             myHealthValue = Integer.parseInt(healthValue);
@@ -24,14 +38,25 @@ public class HealthBuilder implements StrategyBuilder {
         return this;
     }
 
+    /**
+     * @return type of cost strategy to be built
+     */
     public String getType() {
         return myType;
     }
 
+    /**
+     * @return starting health of the sprite
+     */
     public int getHealthValue() {
         return myHealthValue;
     }
 
+    /**
+     * Calls the constructor of the type it's meant to handle; uses default when no type is set
+     * @return a strategy of the specified type with the parameters previously parsed
+     * @throws GameEngineException
+     */
     @Override
     public HealthStrategy build() throws GameEngineException {
         if (myType == null) {
