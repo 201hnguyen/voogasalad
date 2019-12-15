2f1a5aee5adc6feb1949e6d57532cff3ef3a6025
Amber Johnson <ajj18@duke.edu>
2019-12-15 17:56:08 -0500

    more javadoc comments

--- src/voogasalad/gameauthoringenvironment/gui/utilconfig/TabVBoxCreator.java
+++ src/voogasalad/gameauthoringenvironment/gui/utilconfig/TabVBoxCreator.java
@@ -7,7 +7,20 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-
+/**
+ * purpose (why would anyone use it)
+ * assumptions (what situations or values might cause it to fail)
+ * dependencies (what other classes or packages it depends on)
+ * an example of how to use it
+ * any other details users should know
+ * This class is used for creating a VBox in the Tower, Enemy, and Level tabs.
+ * It assumes that someone using this class would want a title and padding added to the VBox.
+ * It is not dependent on other classes or methods, except for where it is instantiate.
+ * Example:
+ *          TabVBoxCreator vbox = new TabVBoxCreator("Example VBox", 250, 10, 10, 10, 10);
+ *
+ * @author Amber Johnson
+ */
 public class TabVBoxCreator extends VBox{
 
     public TabVBoxCreator(String vBoxName, double width, double top, double right, double bottom, double left) {
